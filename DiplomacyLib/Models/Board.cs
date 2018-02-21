28d934acaf3726362057546ab09874ecec2537ac
vfridell <gusknows@gmail.com>
2018-02-21 17:02:44 -0500

    did stuff

--- DiplomacyLib/Models/Board.cs
+++ DiplomacyLib/Models/Board.cs
@@ -8,6 +8,17 @@ namespace DiplomacyLib.Models
 {
     public class Board
     {
+        public Dictionary<Territory, Unit> OccupiedTerritories { get; protected set; }
+
+        public bool IsOccupied(Territory t) => OccupiedTerritories.ContainsKey(t);
+        public bool IsUnoccupied(Territory t) => !IsOccupied(t);
+        public IEnumerable<Unit> Units(Powers power) => OccupiedTerritories.Where(kvp => kvp.Value.Power == power).Select(kvp => kvp.Value);
+
+        public static Board GetInitialBoard()
+        {
+            throw new NotImplementedException();
+        }
+
 
     }
 }
--- DiplomacyLib/Models/Unit.cs
+++ DiplomacyLib/Models/Unit.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace DiplomacyLib.Models
 {
-    public class Unit
+    public abstract class Unit
     {
+        public readonly Powers Power;
+
+        internal Unit(Powers power)
+        {
+            Power = power;
+        }
+
+        public abstract bool TerritoryCompatible(Territory t);
+    }
+
+    public class Army : Unit
+    {
+        internal Army(Powers power) : base(power) { }
+
+        public override bool TerritoryCompatible(Territory t) => t.TerritoryType == TerritoryType.Inland || t.TerritoryType == TerritoryType.Coast;
+    }
+
+    public class Fleet : Unit
+    {
+        internal Fleet(Powers power) : base(power) { }
+
+        public override bool TerritoryCompatible(Territory t) => t.TerritoryType == TerritoryType.Sea || t.TerritoryType == TerritoryType.Coast;
     }
 }
