19df9feda652943564b1041820342a581e880d46
vfridell <gusknows@gmail.com>
2018-04-05 10:34:10 -0400

    modeled alliance scenario

--- DiplomacyLib/AI/AllianceScenario.cs
+++ DiplomacyLib/AI/AllianceScenario.cs
@@ -4,11 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuickGraph;
 
 namespace DiplomacyLib.AI
 {
-    public class AllianceScenario : Dictionary<Powers, Alliance>
+    public class AllianceEdge : Edge<Powers>
     {
+        public double Animosity { get; set; }
+        public AllianceEdge(Powers source, Powers target, double animosity) : base(source, target)
+        {
+            Animosity = animosity;
+        }
+    }
 
+    public class AllianceScenario : BidirectionalGraph<Powers, AllianceEdge>
+    {
+        public AllianceScenario()
+        {
+            Random random = new Random(25422245);
+            for (int i = 1; i < 8; i++) AddVertex((Powers)i);
+
+            for(int i=1; i<8; i++)
+            {
+                for (int j = 1; j < 8; j++)
+                {
+                    if (i == j) continue;
+                    var edge = new AllianceEdge((Powers)i, (Powers)j, random.NextDouble());
+                    AddEdge(edge);
+                }
+            }
+        }
     }
 }
---
+++ DiplomacyTests/AllianceTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DiplomacyLib.AI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiplomacyTests
+{
+    [TestClass]
+    public class AllianceTests
+    {
+        [TestMethod]
+        public void AllianceScenarioInit()
+        {
+            AllianceScenario allianceScenario = new AllianceScenario();
+            Assert.AreEqual(42, allianceScenario.EdgeCount);
+            Assert.AreEqual(7, allianceScenario.VertexCount);
+        }
+    }
+}
