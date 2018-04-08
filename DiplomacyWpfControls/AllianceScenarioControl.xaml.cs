b1a6013a90869e71d19e4c53dd2180c08bb79ce9
vfridell <gusknows@gmail.com>
2018-04-08 16:19:56 -0400

    misfire?

---
+++ DiplomacyWpfControls/AllianceScenarioControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DiplomacyLib.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiplomacyWpfControls
+{
+    /// <summary>
+    /// Interaction logic for AllianceScenarioControl.xaml
+    /// </summary>
+    public partial class AllianceScenarioControl : UserControl
+    {
+        public AllianceScenarioControl()
+        {
+            InitializeComponent();
+        }
+
+        private AllianceScenario _allianceScenario;
+        public AllianceScenario AllianceScenario
+        {
+            get { return _allianceScenario; }
+            set { _allianceScenario = value; }
+        }
+
+        public void Refresh()
+        {
+            if (null == _allianceScenario) return;
+
+
+        }
+    }
+}
---
+++ DiplomacyWpfControls/Drawing/Hexagon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DiplomacyWpfControls.Drawing
+{
+    public static class Hexagon 
+    {
+        public static Polygon Get()
+        {
+            return Get(1, new Point(0, 0));
+        }
+
+        public static Polygon Get(double size, Point center)
+        {
+            var polygon = new Polygon();
+            for (int i = 1; i <= 6; i++)
+            {
+                polygon.Points.Add(HexCorner(center, size, i));
+            }
+            return polygon;
+        }
+
+        private static Point HexCorner(Point center, double size, int corner_number)
+        {
+            double angle_deg = 60 * corner_number + 30;
+            double angle_rad = Math.PI / 180 * angle_deg;
+            return new Point(center.X + size * Math.Cos(angle_rad), center.Y + size * Math.Sin(angle_rad));
+        }
+    }
+}
